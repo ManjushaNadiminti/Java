230142aca1a621fed418cf76664bcf0944c6b71b
ManjushaNadiminti <Nadimintimanjusha@gmail.com>
2024-12-23 16:34:02 +0530

    Create primitive data types.java

---
+++ primitive data types.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Pd{
+    static boolean a;
+    static byte b;
+    static char c;
+    static short d;
+    static int e;
+    static float f;
+    static long g;
+    static double h;
+public static void main(String args[])
+{
+    System.out.println("boolean:"+a);
+    System.out.println("byte:"+b);
+    System.out.println("char:"+c);
+    System.out.println("short:"+d);
+    System.out.println("int:"+e);
+    System.out.println("float:"+f);
+    System.out.println("long:"+g);
+    System.out.println("double:"+h);
+}
+}
+
+OUTPUT: 
+      boolean:false
+      byte:0
+      char:
+      short:0
+      int:0
+      float:0.0
+      double:0.0
